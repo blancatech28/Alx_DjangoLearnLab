7b299e8c99eb48b1a9af0b38a112a5120d81894d
Blancatech <Ebenezergyimah52@gmail.com>
2025-11-14 16:13:27 +0000

    Added custom user to bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -8,6 +11,19 @@ class BookAdmin(admin.ModelAdmin):
 
     
 admin.site.register(Book, BookAdmin)
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -4,22 +4,6 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
-
-
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
