3a701223fe5a605b5ba818d4b211f23a606b8750
Blancatech <Ebenezergyimah52@gmail.com>
2025-11-14 20:22:32 +0000

    fix

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm,ExampleForm
 
 # Views are protected with permission_required decorators
 # Users must have the correct permission to access these views
