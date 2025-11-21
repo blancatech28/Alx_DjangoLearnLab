74d25ea68944c083fe0ceb65dac5ca08ea9ec265
Blancatech <Ebenezergyimah52@gmail.com>
2025-11-21 19:21:48 +0000

    implemented CRUD operations with Viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
 
 # Create your views here.
