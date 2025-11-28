8302dbe95f2a4e3d7811c0a3a0f2d1fbbab2d84b
Blancatech <Ebenezergyimah52@gmail.com>
2025-11-28 12:27:51 +0000

    Added generics import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from rest_framework import generics
 
 # -----------------------------
 # Custom Pagination Class
