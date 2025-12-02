ba09f03832b1a01a27d5cab5c25bb5aaf67ccb60
Blancatech <Ebenezergyimah52@gmail.com>
2025-12-02 15:40:34 +0000

    Initial setup for blog project

---
+++ django_blog/blog/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if not password:
+            raise ValueError("Superusers must have a password")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+
+    # Required fields
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []   # No username
+
+    def __str__(self):
+        return self.email
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200,null=False,blank=False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='posts')
+    content = models.TextField()
+    Published_date = models.DateTimeField(auto_now_add=True)
+  
+
+
+# Create your models here.
