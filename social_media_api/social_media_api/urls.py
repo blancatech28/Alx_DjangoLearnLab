7948ba455268ef0960ddffffaf76bf4f8659c23e
Blancatech <Ebenezergyimah52@gmail.com>
2025-12-08 17:26:39 +0000

    change post url in project urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
-    path('post/', include('posts.urls')),
+    path('api/', include('posts.urls')),
     path('notification/', include('notifications.urls')),
 ]
